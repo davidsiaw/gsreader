94c79994a260179c72d8585415dbfe4746f05ce8
David Siaw <874280+davidsiaw@users.noreply.github.com>
2026-06-11 14:06:56 +0900

    clean up tests

--- spec/gs_reader/gs_reader_spec.rb
+++ spec/gs_reader/gs_reader_spec.rb
@@ -20,12 +20,9 @@ RSpec.describe GsReader do
   end
 
   describe '.build_credentials' do
-    let(:credentials_obj) do
-      instance_double(Google::Auth::UserRefreshCredentials,
-                      fetch_access_token!: { 'access_token' => 't' })
-    end
-
     it 'returns passthrough for credentials objects' do
+      credentials_obj = instance_double(Google::Auth::UserRefreshCredentials,
+                                        fetch_access_token!: { 'access_token' => 't' })
       expect(described_class.build_credentials(credentials_obj, scope: GsReader::READ_SCOPE)).to eq(credentials_obj)
     end
 
@@ -38,10 +35,10 @@ RSpec.describe GsReader do
     end
   end
 
-  # rubocop:disable RSpec/MultipleMemoizedHelpers
   describe 'API methods' do
-    let(:spreadsheet_id) { 'test_spreadsheet_id' }
-    let(:credentials) { { 'type' => 'service_account', 'client_email' => 'test@example.com' } }
+    let(:credentials) do
+      { 'type' => 'service_account', 'client_email' => 'test@example.com' }
+    end
     let(:client_options) { double(application_name: nil) }
     let(:service) do
       allow(client_options).to receive(:application_name=)
@@ -49,40 +46,41 @@ RSpec.describe GsReader do
       allow(d).to receive(:authorization=)
       d
     end
-    let(:expected_range) { Google::Apis::SheetsV4::ValueRange.new(values: [%w[a b]]) }
-    let(:expected_spreadsheet) do
-      Google::Apis::SheetsV4::Spreadsheet.new(
-        properties: Google::Apis::SheetsV4::SpreadsheetProperties.new(title: 'Test Sheet')
-      )
-    end
-    let(:sheets) do
-      [Google::Apis::SheetsV4::Sheet.new(
-        properties: Google::Apis::SheetsV4::SheetProperties.new(title: 'Sheet1')
-      )]
-    end
-    let(:sheet_titles_response) { Google::Apis::SheetsV4::Spreadsheet.new(sheets: sheets) }
 
     before do
       allow(described_class).to receive(:build_credentials).and_return(credentials)
     end
 
     it 'has get_range method' do
-      allow(service).to receive(:get_spreadsheet_values).and_return(expected_range)
-      reader = GsReader::Reader.new(spreadsheet_id, credentials, service: service)
+      allow(service).to receive(:get_spreadsheet_values).and_return(
+        Google::Apis::SheetsV4::ValueRange.new(values: [%w[a b]])
+      )
+      reader = GsReader::Reader.new('test_spreadsheet_id', credentials, service: service)
       expect(reader.get_range('A1:B1')).to be_a(Google::Apis::SheetsV4::ValueRange)
     end
 
     it 'has spreadsheet method' do
-      allow(service).to receive(:get_spreadsheet).and_return(expected_spreadsheet)
-      reader = GsReader::Reader.new(spreadsheet_id, credentials, service: service)
+      allow(service).to receive(:get_spreadsheet).and_return(
+        Google::Apis::SheetsV4::Spreadsheet.new(
+          properties: Google::Apis::SheetsV4::SpreadsheetProperties.new(title: 'Test Sheet')
+        )
+      )
+      reader = GsReader::Reader.new('test_spreadsheet_id', credentials, service: service)
       expect(reader.spreadsheet).to be_a(Google::Apis::SheetsV4::Spreadsheet)
     end
 
     it 'has sheet_titles method' do
-      allow(service).to receive(:get_spreadsheet).and_return(sheet_titles_response)
-      reader = GsReader::Reader.new(spreadsheet_id, credentials, service: service)
+      allow(service).to receive(:get_spreadsheet).and_return(
+        Google::Apis::SheetsV4::Spreadsheet.new(
+          sheets: [
+            Google::Apis::SheetsV4::Sheet.new(
+              properties: Google::Apis::SheetsV4::SheetProperties.new(title: 'Sheet1')
+            )
+          ]
+        )
+      )
+      reader = GsReader::Reader.new('test_spreadsheet_id', credentials, service: service)
       expect(reader.sheet_titles).to eq(%w[Sheet1])
     end
   end
-  # rubocop:enable RSpec/MultipleMemoizedHelpers
 end
--- spec/gs_reader/sheet_spec.rb
+++ spec/gs_reader/sheet_spec.rb
@@ -3,8 +3,9 @@
 require 'google/apis/sheets_v4'
 
 RSpec.describe GsReader::Sheet do
-  let(:spreadsheet_id) { 'test_spreadsheet_id' }
-  let(:credentials) { { 'type' => 'service_account', 'client_email' => 'test@example.com' } }
+  let(:credentials) do
+    { 'type' => 'service_account', 'client_email' => 'test@example.com' }
+  end
   let(:client_options) { double(application_name: nil) }
   let(:service) do
     allow(client_options).to receive(:application_name=)
@@ -17,116 +18,117 @@ RSpec.describe GsReader::Sheet do
     allow(GsReader).to receive(:build_credentials).and_return(credentials)
   end
 
-  # rubocop:disable RSpec/MultipleMemoizedHelpers
-  describe '#get_range' do
-    let(:expected_range) { Google::Apis::SheetsV4::ValueRange.new(values: [%w[a b], %w[c d]]) }
-    let(:sheet) { described_class.new(spreadsheet_id, credentials, scope: GsReader::READ_SCOPE, service: service) }
-
-    before do
-      allow(service).to receive(:get_spreadsheet_values).and_return(expected_range)
-    end
-
-    it 'returns a ValueRange' do
-      expect(sheet.get_range('A1:B2')).to be_a(Google::Apis::SheetsV4::ValueRange)
-    end
-
-    it 'returns the correct values' do
-      expect(sheet.get_range('A1:B2').values).to eq([%w[a b], %w[c d]])
-    end
+  it 'returns a ValueRange from get_range' do
+    allow(service).to receive(:get_spreadsheet_values).and_return(
+      Google::Apis::SheetsV4::ValueRange.new(values: [%w[a b], %w[c d]])
+    )
+    sheet = described_class.new('test_spreadsheet_id', credentials,
+                                scope: GsReader::READ_SCOPE, service: service)
+    expect(sheet.get_range('A1:B2')).to be_a(Google::Apis::SheetsV4::ValueRange)
   end
-  # rubocop:enable RSpec/MultipleMemoizedHelpers
 
-  # rubocop:disable RSpec/MultipleMemoizedHelpers
-  describe '#spreadsheet' do
-    let(:expected_spreadsheet) do
+  it 'returns the correct values from get_range' do
+    allow(service).to receive(:get_spreadsheet_values).and_return(
+      Google::Apis::SheetsV4::ValueRange.new(values: [%w[a b], %w[c d]])
+    )
+    sheet = described_class.new('test_spreadsheet_id', credentials,
+                                scope: GsReader::READ_SCOPE, service: service)
+    expect(sheet.get_range('A1:B2').values).to eq([%w[a b], %w[c d]])
+  end
+
+  it 'returns spreadsheet metadata' do
+    allow(service).to receive(:get_spreadsheet).and_return(
       Google::Apis::SheetsV4::Spreadsheet.new(
         properties: Google::Apis::SheetsV4::SpreadsheetProperties.new(title: 'Test Sheet')
       )
-    end
-    let(:sheet) { described_class.new(spreadsheet_id, credentials, scope: GsReader::READ_SCOPE, service: service) }
-
-    before do
-      allow(service).to receive(:get_spreadsheet).and_return(expected_spreadsheet)
-    end
-
-    it 'returns spreadsheet metadata' do
-      expect(sheet.spreadsheet.properties.title).to eq('Test Sheet')
-    end
-  end
-  # rubocop:enable RSpec/MultipleMemoizedHelpers
-
-  # rubocop:disable RSpec/MultipleMemoizedHelpers
-  describe '#sheet_titles' do
-    let(:sheets) do
-      [
-        Google::Apis::SheetsV4::Sheet.new(
-          properties: Google::Apis::SheetsV4::SheetProperties.new(title: 'Sheet1')
-        ),
-        Google::Apis::SheetsV4::Sheet.new(
-          properties: Google::Apis::SheetsV4::SheetProperties.new(title: 'Sheet2')
-        )
-      ]
-    end
-    let(:expected_spreadsheet) { Google::Apis::SheetsV4::Spreadsheet.new(sheets: sheets) }
-    let(:sheet) { described_class.new(spreadsheet_id, credentials, scope: GsReader::READ_SCOPE, service: service) }
-
-    before do
-      allow(service).to receive(:get_spreadsheet).and_return(expected_spreadsheet)
-    end
-
-    it 'returns an array' do
-      expect(sheet.sheet_titles).to be_an(Array)
-    end
-
-    it 'returns the correct titles' do
-      expect(sheet.sheet_titles).to eq(%w[Sheet1 Sheet2])
-    end
-  end
-  # rubocop:enable RSpec/MultipleMemoizedHelpers
-
-  describe '#single_cell?' do
-    let(:sheet) { described_class.new(spreadsheet_id, credentials, scope: GsReader::READ_SCOPE, service: service) }
-
-    it 'recognizes A1' do
-      expect(sheet.send(:single_cell?, 'A1')).to be true
-    end
-
-    it 'recognizes Sheet1!A1' do
-      expect(sheet.send(:single_cell?, 'Sheet1!A1')).to be true
-    end
-
-    it 'recognizes A1:B2 as multi-cell' do
-      expect(sheet.send(:single_cell?, 'A1:B2')).to be false
-    end
-
-    it 'recognizes Sheet1!A1:B2 as multi-cell' do
-      expect(sheet.send(:single_cell?, 'Sheet1!A1:B2')).to be false
-    end
+    )
+    sheet = described_class.new('test_spreadsheet_id', credentials,
+                                scope: GsReader::READ_SCOPE, service: service)
+    expect(sheet.spreadsheet.properties.title).to eq('Test Sheet')
   end
 
-  describe '#qualify_range' do
-    let(:sheet) { described_class.new(spreadsheet_id, credentials, scope: GsReader::READ_SCOPE, service: service) }
-
-    it 'adds default sheet title' do
-      sheet = described_class.new(spreadsheet_id, credentials, scope: GsReader::READ_SCOPE,
-                                                               sheet: 'Sheet1', service: service)
-      expect(sheet.send(:qualify_range, 'A1')).to eq("'Sheet1'!A1")
-    end
-
-    it 'preserves existing sheet qualifier' do
-      expect(sheet.send(:qualify_range, 'Sheet1!A1')).to eq('Sheet1!A1')
-    end
+  it 'sheet_titles returns an array' do
+    allow(service).to receive(:get_spreadsheet).and_return(
+      Google::Apis::SheetsV4::Spreadsheet.new(
+        sheets: [
+          Google::Apis::SheetsV4::Sheet.new(
+            properties: Google::Apis::SheetsV4::SheetProperties.new(title: 'Sheet1')
+          ),
+          Google::Apis::SheetsV4::Sheet.new(
+            properties: Google::Apis::SheetsV4::SheetProperties.new(title: 'Sheet2')
+          )
+        ]
+      )
+    )
+    sheet = described_class.new('test_spreadsheet_id', credentials,
+                                scope: GsReader::READ_SCOPE, service: service)
+    expect(sheet.sheet_titles).to be_an(Array)
   end
 
-  describe '#quote_sheet_title' do
-    let(:sheet) { described_class.new(spreadsheet_id, credentials, scope: GsReader::READ_SCOPE, service: service) }
+  it 'sheet_titles returns the correct titles' do
+    allow(service).to receive(:get_spreadsheet).and_return(
+      Google::Apis::SheetsV4::Spreadsheet.new(
+        sheets: [
+          Google::Apis::SheetsV4::Sheet.new(
+            properties: Google::Apis::SheetsV4::SheetProperties.new(title: 'Sheet1')
+          ),
+          Google::Apis::SheetsV4::Sheet.new(
+            properties: Google::Apis::SheetsV4::SheetProperties.new(title: 'Sheet2')
+          )
+        ]
+      )
+    )
+    sheet = described_class.new('test_spreadsheet_id', credentials,
+                                scope: GsReader::READ_SCOPE, service: service)
+    expect(sheet.sheet_titles).to eq(%w[Sheet1 Sheet2])
+  end
 
-    it 'wraps titles with spaces' do
-      expect(sheet.send(:quote_sheet_title, 'Sheet with spaces')).to eq("'Sheet with spaces'")
-    end
+  it 'recognizes A1 as a single cell' do
+    sheet = described_class.new('test_spreadsheet_id', credentials,
+                                scope: GsReader::READ_SCOPE, service: service)
+    expect(sheet.send(:single_cell?, 'A1')).to be true
+  end
 
-    it 'doubles single quotes' do
-      expect(sheet.send(:quote_sheet_title, "Sheet's title")).to eq("'Sheet''s title'")
-    end
+  it 'recognizes Sheet1!A1 as a single cell' do
+    sheet = described_class.new('test_spreadsheet_id', credentials,
+                                scope: GsReader::READ_SCOPE, service: service)
+    expect(sheet.send(:single_cell?, 'Sheet1!A1')).to be true
+  end
+
+  it 'recognizes A1:B2 as multi-cell' do
+    sheet = described_class.new('test_spreadsheet_id', credentials,
+                                scope: GsReader::READ_SCOPE, service: service)
+    expect(sheet.send(:single_cell?, 'A1:B2')).to be false
+  end
+
+  it 'recognizes Sheet1!A1:B2 as multi-cell' do
+    sheet = described_class.new('test_spreadsheet_id', credentials,
+                                scope: GsReader::READ_SCOPE, service: service)
+    expect(sheet.send(:single_cell?, 'Sheet1!A1:B2')).to be false
+  end
+
+  it 'adds default sheet title in qualify_range' do
+    sheet = described_class.new('test_spreadsheet_id', credentials,
+                                scope: GsReader::READ_SCOPE, sheet: 'Sheet1',
+                                service: service)
+    expect(sheet.send(:qualify_range, 'A1')).to eq("'Sheet1'!A1")
+  end
+
+  it 'preserves existing sheet qualifier in qualify_range' do
+    sheet = described_class.new('test_spreadsheet_id', credentials,
+                                scope: GsReader::READ_SCOPE, service: service)
+    expect(sheet.send(:qualify_range, 'Sheet1!A1')).to eq('Sheet1!A1')
+  end
+
+  it 'wraps titles with spaces in quote_sheet_title' do
+    sheet = described_class.new('test_spreadsheet_id', credentials,
+                                scope: GsReader::READ_SCOPE, service: service)
+    expect(sheet.send(:quote_sheet_title, 'Sheet with spaces')).to eq("'Sheet with spaces'")
+  end
+
+  it 'doubles single quotes in quote_sheet_title' do
+    sheet = described_class.new('test_spreadsheet_id', credentials,
+                                scope: GsReader::READ_SCOPE, service: service)
+    expect(sheet.send(:quote_sheet_title, "Sheet's title")).to eq("'Sheet''s title'")
   end
 end
